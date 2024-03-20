c5cc4a526e7302e7262a9566c1ff52415dd59f7d
KaviMaduwage <kavindimaduwage@gmail.com>
2024-03-20 16:26:37 -0700

    added passenger dash board

---
+++ src/components/PassengerDashBoard.js
@@ -0,0 +1,10 @@
+export default function PassengerDashBoard({ loggedIn }){
+    return (
+        <div>
+
+            <h1>Passenger Dashboard {loggedIn}</h1>
+
+            <h3>Welcome</h3>
+        </div>
+    )
+}
