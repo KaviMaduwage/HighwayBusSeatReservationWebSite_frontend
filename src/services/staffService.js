3389c9e6562658581ceb90b8178077f5050a8da7
KaviMaduwage <kavindimaduwage@gmail.com>
2024-04-11 17:45:28 -0700

    added search staff functionality

--- src/services/staffService.js
+++ src/services/staffService.js
@@ -8,4 +8,6 @@ export const saveStaffMember = (busCrew) => axios.post(API_BASE_URL+"/saveBusCre
 export const populateStaffDetails = (user) => axios.post(API_BASE_URL+"/loadAllBusCrewDetailsByBusOwnerUserId", user);
 export const findMemberById = (memberId) => axios.post(API_BASE_URL+"/findMemberById", { memberId: memberId });
 
-export const deleteMemberById = (memberId) => axios.post(API_BASE_URL+"/deleteMemberById", { memberId: memberId });
+export const deleteMemberById = (memberId, crewTypeId) => axios.post(API_BASE_URL+"/deleteMemberById", { memberId: memberId, crewTypeId:crewTypeId });
+
+export const searchStaff = (name,jobType, status,userId) => axios.post(API_BASE_URL+"/searchMember",{name : name, jobType : jobType, status:status, userId:userId})
